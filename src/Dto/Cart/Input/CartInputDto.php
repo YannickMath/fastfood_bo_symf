c5ef0288f7221c32c97f76da6e1280c2aefb248a
Yannick MATH <yannick_math@yahoo.fr>
2025-11-29 09:13:46 +0100

    add constraints validation in Dto

--- src/Dto/Cart/Input/CartInputDto.php
+++ src/Dto/Cart/Input/CartInputDto.php
@@ -1,10 +1,14 @@
 <?php
 namespace App\Dto\Cart\Input;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class CartInputDto
 {
     /**
      * @var CartItemInputDto[]
      */
+    #[Assert\NotBlank(message: "Cart cannot be empty")]
+    #[Assert\Valid]
     public array $items = [];
 }
--- src/Dto/cartItem/Input/cartItemInputDto.php
+++ src/Dto/cartItem/Input/cartItemInputDto.php
@@ -2,10 +2,17 @@
 
 namespace App\Dto\Cart\Input;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class CartItemInputDto
 {
-public function __construct(
-public int $productId,
-public int $quantity
-) {}
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Positive]
+        public int $productId,
+
+        #[Assert\NotBlank]
+        #[Assert\Positive]
+        public int $quantity
+    ) {}
 }
